9d2a9e5171a8cd47cda8371a78722eb74fdb99e5
Zack Carlson <jzcarlson014@gmail.com>
2020-08-25 03:02:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,10 +7,8 @@ def join_nested_strings(src)
       if src[row_index][element_index].class == String
         new_string + ' ' + '#{src[row_index][element_index]}'
       end
+      element_index += 1
     end
+    row_index += 1
   end
-
-
-
-
 end
